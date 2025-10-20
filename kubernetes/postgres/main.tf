758998f2b19c1de1b5d4678dadbe0c3bf127a635
Reshma Abdul Rahim <61033581+Reshrahim@users.noreply.github.com>
2025-10-20 17:04:56 -0400

    Update PostgreSQL module and memory settings

--- kubernetes/postgres/main.tf
+++ kubernetes/postgres/main.tf
@@ -2,27 +2,27 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0"
+      version = "= 2.16.1"
     }
   }
 }
 
 variable "context" {
   description = "This variable contains Radius recipe context."
-  type = any
+  type        = any
 }
 
 variable "memory" {
   description = "Memory limits for the PostgreSQL container"
   type = map(object({
     memoryRequest = string
-    memoryLimit  = string
+    memoryLimit   = string
   }))
   default = {
     S = {
       memoryRequest = "512Mi"
       memoryLimit   = "1024Mi"
-    },
+    }
     M = {
       memoryRequest = "1Gi"
       memoryLimit   = "2Gi"
@@ -31,35 +31,30 @@ variable "memory" {
 }
 
 locals {
-  uniqueName      = var.context.resource.name
-  namespace       = var.context.runtime.kubernetes.namespace
-  username        = "postgres"
-  memory_settings = try(var.memory[var.context.resource.properties.size], var.memory["S"])
+  uniqueName = var.context.resource.name
+  namespace  = var.context.runtime.kubernetes.namespace
+  username   = "postgres"
 }
 
 module "postgresql" {
-  source  = "ballj/postgresql/kubernetes"
-  version = "~> 1.2"
+  source        = "ballj/postgresql/kubernetes"
+  version       = "~> 1.2"
   namespace     = local.namespace
   object_prefix = local.uniqueName
   name          = local.uniqueName
   username      = local.username
-  resources_requests_memory = local.memory_settings.memoryRequest
-  resources_limits_memory   = local.memory_settings.memoryLimit
-  password_key   = "password"
-  image_name = "postgres"
-  image_tag  = "16"
+  password_key  = "password"
 }
 
 output "result" {
   value = {
     values = {
-      host            = "${module.postgresql.hostname}.${local.namespace}.svc.cluster.local"
-      port            = module.postgresql.port
-      database        = module.postgresql.name
-      username        = module.postgresql.username
-      password_key    = module.postgresql.password_key
-      password_secret = module.postgresql.password_secret
+      host = "${kubernetes_service.postgresql.metadata[0].name}.${kubernetes_service.postgresql.metadata[0].namespace}.svc.cluster.local"
+      port = "${kubernetes_service.postgresql.spec[0].port[0].port}"
+      database = local.uniqueName
+      username = local.username
+      password_key = "password"
+      password_secret = "${kubernetes_secret.postgresql[0].metadata[0].name}"
     }
   }
 }
