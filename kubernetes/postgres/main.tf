0ea9fae3e934254955f8199043f2ebbaef0ed2cb
Reshma Abdul Rahim <61033581+Reshrahim@users.noreply.github.com>
2025-10-20 16:39:14 -0400

    Refactor PostgreSQL configuration with module
    Refactor PostgreSQL deployment to use a module and remove hardcoded resources.

--- kubernetes/postgres/main.tf
+++ kubernetes/postgres/main.tf
@@ -32,95 +32,28 @@ variable "memory" {
 
 locals {
   uniqueName = var.context.resource.name
-  port     = 5432
   namespace = var.context.runtime.kubernetes.namespace
+  username = "postgres"
 }
 
-resource "random_password" "password" {
-  length           = 16
-  special          = true
-  override_special = "!@#^&*()-_=+"
-}
-
-resource "kubernetes_deployment" "postgresql" {
-  metadata {
-    name      = local.uniqueName
-    namespace = local.namespace
-  }
-
-  spec {
-    selector {
-      match_labels = {
-        app = "postgres"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "postgres"
-        }
-      }
-
-      spec {
-        container {
-          image = "postgres:16-alpine"
-          name  = "postgres"
-          resources {
-            requests = {
-              memory = var.memory[var.context.resource.properties.size].memoryRequest
-              }
-              limits = {
-                memory= var.memory[var.context.resource.properties.size].memoryLimit
-              }
-            }
-          env {
-            name  = "POSTGRES_PASSWORD"
-            value = random_password.password.result
-          }
-          env {
-            name = "POSTGRES_USER"
-            value = "postgres"
-          }
-          env {
-            name  = "POSTGRES_DB"
-            value = "postgres_db"
-          }
-          port {
-            container_port = local.port
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "postgres" {
-  metadata {
-    name      = local.uniqueName
-    namespace = local.namespace
-  }
-
-  spec {
-    selector = {
-      app = "postgres"
-    }
-
-    port {
-      port        = local.port
-      target_port = local.port
-    } 
-  }
+module "postgresql" {
+  source        = "ballj/postgresql/kubernetes"
+  version       = "~> 1.2"
+  namespace     = namespace
+  object_prefix = "myapp-db"
+  name          = uniqueName
+  password_key = "password"
 }
 
 output "result" {
   value = {
     values = {
-      host = "${kubernetes_service.postgres.metadata[0].name}.${kubernetes_service.postgres.metadata[0].namespace}.svc.cluster.local"
-      port = local.port
-      database = "postgres_db"
-      username = "postgres"
-      password = random_password.password.result
+      host = "${kubernetes_service.postgresql.metadata[0].name}.${kubernetes_service.postgresql.metadata[0].namespace}.svc.cluster.local"
+      port = "${kubernetes_service.postgresql.spec[0].port[0].port}"
+      database = local.uniqueName
+      username = local.username
+      password_key = "password"
+      password_secret = "${kubernetes_secret.postgresql[0].metadata[0].name}"
     }
   }
 }
