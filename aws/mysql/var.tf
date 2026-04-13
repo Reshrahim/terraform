d00d8647895ece0921e314299b4f8a6b8f238fa4
Reshma Abdul Rahim <61033581+Reshrahim@users.noreply.github.com>
2026-04-13 09:38:01 -0700

    Update var.tf

--- aws/mysql/var.tf
+++ aws/mysql/var.tf
@@ -3,17 +3,7 @@ variable "context" {
   type        = any
 }
 
-variable "subnetIds" {
-  description = "List of subnet IDs for the RDS subnet group. Should be subnets in the EKS cluster's VPC."
-  type        = list(string)
-}
-
-variable "vpcId" {
-  description = "VPC ID where the EKS cluster is running."
-  type        = string
-}
-
-variable "securityGroupId" {
-  description = "Security group ID that allows access from the EKS cluster nodes."
+variable "eksClusterName" {
+  description = "Name of the EKS cluster. Used to discover VPC, subnets, and security groups."
   type        = string
 }
