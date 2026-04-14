61ddb340b5b936952b6fa986571e56e8d84c595d
Reshma Abdul Rahim <61033581+Reshrahim@users.noreply.github.com>
2026-04-14 12:45:43 -0700

    Update var.tf

--- aws/mysql/var.tf
+++ aws/mysql/var.tf
@@ -7,3 +7,15 @@ variable "eksClusterName" {
   description = "Name of the EKS cluster. Used to discover VPC, subnets, and security groups."
   type        = string
 }
+
+variable "subnetGroupName" {
+  description = "Name of an existing DB subnet group. If not provided, one is created using EKS cluster subnets."
+  type        = string
+  default     = ""
+}
+
+variable "vpcSecurityGroupIds" {
+  description = "List of VPC security group IDs for the RDS instance. If not provided, the EKS cluster security group is used."
+  type        = list(string)
+  default     = []
+}
